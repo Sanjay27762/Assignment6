f1b61a494c7e2edc37519884248e1b082f3684e9
SagarVaiyata <53462108+SagarVaiyata@users.noreply.github.com>
2021-11-18 00:10:50 +0530

    Add project files.

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            if(string.IsNullOrEmpty(user))
+            {
+                user = "Anonymous";
+            }
+
+           
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
